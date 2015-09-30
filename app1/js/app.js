600175e66db2226c424b2b7b09f47a3873577728
RickySSD <ricky6982@gmail.com>
2015-09-30 16:22:42 -0300

    Se crearon las diferentes secciones del prototipo. Y se esta utilizando angular-ui-router para acceder a las diferentes secciones.
    Signed-off-by: RickySSD <ricky6982@gmail.com>

--- app1/js/app.js
+++ app1/js/app.js
@@ -1,4 +1,26 @@
-var app = angular.module('app', []);
+var app = angular.module('app', ['ui.router']);
+
+app.config([
+    '$stateProvider', '$urlRouterProvider',
+    function($stateProvider, $urlRouterProvider){
+        $urlRouterProvider.otherwise('/home');
+
+        $stateProvider
+            .state('home', {
+                url: '/home',
+                templateUrl: 'templates/menu.html'
+            })
+            .state('info', {
+                url: '/info',
+                templateUrl: 'templates/info.html'
+            })
+            .state('cartaMenu', {
+                url: '/cartaMenu',
+                templateUrl: 'templates/cartaMenu.html'
+            })
+        ;
+    }
+]);
 
 app.controller('AppCtrl', [
     '$scope',
